26cf706032b4b12cd7140f96505b60387b1dbbdd
ayorich <kayodele17@yahoo.com>
2019-05-31 15:15:08 +0100

    finish day 2

---
+++ assignment/Ayorich/assignment1/result.py
@@ -0,0 +1,4 @@
+score = int(input("Enter score: "))
+if score > 70 and score <= 100:
+    print("you got", score, "and ")
+    elif score >
---
+++ assignment/Ayorich/assignment2/Assign-multiplication.py
@@ -0,0 +1,11 @@
+x = int(input("Enter number:"))
+
+y= 1
+
+while y <= 12:
+    print(x, " X ", y, " = ", x * y)
+    
+    y += 1
+
+# for y in range (1, 13):
+#     print(x, " X ", y, " = ", x * y)
